785dc0c9a207084296fedea3737859ffc1cd9c9b
eric-the-geric <eric.gericke2@gmail.com>
2025-04-06 16:50:36 +0200

    added test audio and output dir

--- src/__main__.py
+++ src/__main__.py
@@ -1,22 +1,26 @@
 from . import Recorder, StreamParams, Transcription
+import time
 
 
 def main():
     # Recording
-    stream_params = StreamParams()
-    recorder = Recorder(stream_params, stop_phrase="stop recording")
-    recorder.record("audio.wav")
+    #stream_params = StreamParams()
+    #recorder = Recorder(stream_params, stop_phrase="stop recording")
+    #recorder.record("./src/audio.mp3")
 
     # Transcription
 
     trans = Transcription()
     
     # Example usage
-    trans("audio.wav", "./transcript.txt")
+    start = time.time()
+    trans("src/audio.mp3", "./test.txt")
     
     # Print results
     print("Transcription Text:", trans.result.text)
     print(trans.result)
+    end = time.time()
+    print(end - start)
     # To access timestamps (if using verbose_json):
     if hasattr(trans.result, 'segments'):
         for segment in trans.result.segments:
